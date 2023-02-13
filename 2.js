4b813387c09fb6aea7c1d56328d20a5c14951a72
UsamaAdilGhumman <usama.adil.ghumman@gmail.com>
2023-02-13 19:16:32 +0500

    Change Text textContent & innertext

---
+++ 2.js
@@ -0,0 +1,17 @@
+// Change Text
+
+// textContent property 
+// change text 
+// ya sara ka sara text da ga jo display none b ha wo b text aya ga
+const mainHeading = document.getElementById("main-heading");
+console.log(mainHeading);
+console.log(mainHeading.textContent);
+mainHeading.textContent = "This is something different";
+console.log(mainHeading.textContent);
+
+// innertext property 
+// change text 
+// ya sara ka sara text ni da ga jo display ha wo hi text aya ga
+const mainHeading1 = document.getElementById("main-heading");
+console.log(mainHeading1.innerText);
+
